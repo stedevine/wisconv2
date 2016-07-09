e29dc4abd05d6925293b32512cc5aa2cf4416fc1
Stephen Devine <stephenaturton@hotmail.com>
2016-07-09 15:35:14 -0700

    checkpoint

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 var app = angular.module('app', ['ui.router']);
 
 app.config(
-  function($stateProvider, $urlRouterProvider) {
+		function($stateProvider, $urlRouterProvider) {
+			$urlRouterProvider.otherwise('/');
+			$stateProvider
 
-    $urlRouterProvider.otherwise('/');
-
-    $stateProvider
-
-    .state('hello', {
-      url: "/",
-      templateUrl: 'views/hello.html',
-    })
-
-    .state('about', {
-      url: "/about",
-      templateUrl: 'views/about.html',
-    })
-
-    .state('experience', {
-      url: "/experience",
-      templateUrl: 'views/experience.html',
-    })
-  });
+	.state('hello', {url: "/",templateUrl: 'views/hello.html',})
+	.state('about', {url: "/about", templateUrl: 'views/about.html',})
+	.state('shows', {url: "/shows",templateUrl: 'views/shows.html',})
+	.state('music', {url: "/music",templateUrl: 'views/music.html',})
+	.state('videos', { url: "/videos",templateUrl: 'views/videos.html',})
+		});
